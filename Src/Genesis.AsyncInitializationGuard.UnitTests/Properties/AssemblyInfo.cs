7998963b407dca33a2d0efb2fd1ec4466fd71fd2
Kent Boogaart <kent.boogaart@gmail.com>
2017-04-12 18:31:54 +0930

    Migrate to netstandard

--- Src/Genesis.AsyncInitializationGuard.UnitTests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Genesis.AsyncInitializationGuard.UnitTests")]
-[assembly: AssemblyDescription("Contains unit tests for Genesis.AsyncInitializationGuard.")]
-[assembly: CLSCompliantAttribute(true)]
--- Src/Genesis.AsyncInitializationGuard/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Genesis.AsyncInitializationGuard")]
-[assembly: AssemblyDescription("Contains the implementation of Genesis.AsyncInitializationGuard.")]
-[assembly: CLSCompliantAttribute(true)]
